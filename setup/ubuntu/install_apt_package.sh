beb4c1759e3bb0b91ef993ccaea07439c90c1f7a
Nobuchika Tanaka <codequokka@gmail.com>
2020-08-04 07:42:00 +0900

    Fix conflict

--- setup/ubuntu/install_apt_package.sh
+++ setup/ubuntu/install_apt_package.sh
@@ -7,9 +7,19 @@ sudo apt install -y zsh
 sudo apt install -y neovim
 
 # Vcs
+sudo apt install -y git
 sudo apt install -y git-lfs
+
+# Terminal
+sudo apt install -y tmux
 
 # Python3
 sudo apt install -y python3.8
 sudo apt install -y python3-pip
 sudo apt install -y python3.8-venv
+
+# Virtualizaton
+sudo apt install -y virtualbox
+
+# Utility
+sudo apt install -y curl
