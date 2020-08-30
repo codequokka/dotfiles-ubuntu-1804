41d0e487c12bc421bd95f34a0a5833d92ec51c8c
Nobuchika Tanaka <codequokka@gmail.com>
2020-08-30 17:26:08 +0900

    Add some changes

--- setup/ubuntu/install_apt_package.sh
+++ setup/ubuntu/install_apt_package.sh
@@ -17,6 +17,10 @@ sudo apt install -y tmux
 sudo apt install -y python3.8
 sudo apt install -y python3-pip
 sudo apt install -y python3.8-venv
+sudo apt install -y python3-venv
+
+# Python2
+sudo apt install -y virtualenv
 
 # Virtualizaton
 sudo apt install -y virtualbox
